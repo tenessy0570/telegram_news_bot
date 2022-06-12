d047a07f213ab9a1fe678e7e181ad77f7058f8fa
tenessy0570 <romikp30757@gmail.com>
2022-06-12 16:07:14 +0700

    fixed syntax errors

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 BOT_DESCRIPTION = \
     "I am news bot. Using me you can get news with one command!\n" \
     "Available commands:\n" \
-    "/help -> get all commands description. same can be done with /help\n" \
+    "/help -> get all commands description. Same can be done with /start\n" \
     "/update_n <value> -> set your N value (amount of news headers you will receive)\n" \
     "/get_sources -> get list of all sources. You can choose only one of them!\n" \
     "/set_source -> set source to get news from\n" \
